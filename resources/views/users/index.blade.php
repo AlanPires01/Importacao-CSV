9b56dd4623126cea55885f35590bc4c533dcc100
AlanPires01 <alanpires@alu.ufc.br>
2024-09-25 11:06:26 -0300

    Finalizada a criação do jobs e incluída paginação

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -13,33 +13,54 @@
             <div class="card my-5 border-light shadow ">
                 <h3 class="card-header"> Importar Excel</h3>
 
+                <div class="card-body">
+                    @session('success')
+                        <div class="alert alert-success" role="alert">{!! $value !!}</div>
+                    @endsession
+
+                    @session('error')
+                        <div class="alert alert-danger" role="alert">{!! $value !!}</div>
+                    @endsession
+
+                    @if ($errors->any())
+                        <div class="alert alert-danger" role="alert">
+                            @foreach ($errors->all() as $error)
+                                {{ $error }}
+                            @endforeach
+                        </div>
+                    @endif
+                    <form action="{{route('user.import')}}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="input-group my-4">
+                            <input class="form-control" name="file" type="file" id="file" accept=".csv">
+                            <button type="submit" class="btn btn-outline-success" id="fileBtn"><i class="bi bi-upload me-3"></i>Importar</button>
+                        </div>
+                    </form>
+
+                    <table class="table table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nome</th>
+                                <th>Email</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($users as $user)
+                                <tr>
+                                    <td>{{$user->id}}</td>
+                                    <td>{{$user->name}}</td>
+                                    <td>{{$user->email}}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+
+                    {{$users->links()}}
+                </div>
 
 
-                @session('success')
-                    <div class="alert alert-success" role="alert">{!! $value !!}</div>
-                @endsession
 
-                @session('error')
-                    <div class="alert alert-danger" role="alert">{!! $value !!}</div>
-                @endsession
-
-                @if ($errors->any())
-                    <div class="alert alert-danger" role="alert">
-                        @foreach ($errors->all() as $error)
-                            {{ $error }}
-                        @endforeach
-                    </div>
-                @endif
-
-                <form action="{{route('user.import')}}" method="POST" enctype="multipart/form-data">
-                    @csrf
-                    <div class="input-group my-4">
-                        <input class="form-control" type="file" id="file" accept=".csv">
-                        <button type="submit" class="btn btn-outline-success" id="fileBtn"><i class="bi bi-upload me-3"></i>Importar</button>
-                    </div>
-
-                      
-                </form>
             </div>
         </div>
     </body>
